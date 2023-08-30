c32176205cab2c5ed59523a00414deff6d34b501
john <joherty@gmail.com>
2023-08-30 21:18:40 +0000

    added variables

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,27 +10,19 @@ terraform {
 provider "proxmox" {
   pm_api_url = "https://10.0.0.1:8006/api2/json"
   pm_api_token_id = "terraform@pam!terraform_access"
-
-  # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
   pm_api_token_secret = var.pm_api_token_secret
-
-  # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = true
 }
 
-# resource is formatted to be "[type]" "[entity_name]" so in this case
-# we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "test_server" {
   count = 1 # just want 1 for now, set to 0 and apply to destroy VM
   name = "oak-vm-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named oak-vm-1 in proxmox
 
-  # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
+  # this now reaches out to the vars file.
   target_node = var.proxmox_host
-
-  # another variable with contents "ubuntu-2004-cloudinit-template"
   clone = var.template_name
 
-  # basic VM settings here
+  # basic VM settings
   agent = 1
   os_type = "cloud-init"
   cores = 2
@@ -48,7 +40,7 @@ resource "proxmox_vm_qemu" "test_server" {
     iothread = 1
   }
   
-  # if you want two NICs, just copy this whole network section and duplicate it
+  # if two NICs, just copy this whole network section and duplicate it
   network {
     model = "virtio"
     bridge = "vmbr0"
@@ -60,15 +52,9 @@ resource "proxmox_vm_qemu" "test_server" {
       network,
     ]
   }
-  
-  # the ${count.index + 1} thing appends text to the end of the ip address
-  # in this case, since we are only adding a single VM, the IP will
-  # be 10.98.1.91 since count.index starts at 0. this is how you can create
-  # multiple VMs and have an IP assigned to each (.91, .92, .93, etc.)
 
   ipconfig0 = "ip=10.0.0.2${count.index + 1}/24,gw=10.0.0.254"
   
-  # sshkeys set using variables. the variable contains the text of the key.
   sshkeys = <<EOF
   ${var.ssh_key}
   EOF
